8a796c161f4afa75e8bee4e14c9a69971ab96581
Evgeny <popov-eo@yandex.ru>
2024-02-14 19:26:36 +0300

    Внесены замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,33 @@
 const container = document.querySelector('.content');
 const cardsContainer = container.querySelector('.places__list');
+const deleteButton = container.querySelector('.card__delete-button');
 
-function deleteCard() {
-  document.querySelector('.card').remove();
+function deleteCard(evt) {
+  const listItem = evt.target.closest('.card');
+  listItem.remove();
+};
+
+function createCard(cardData, deleteCallback) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = `Изображение с видом: ${cardData.name}`;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  
+  cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => {
+    evt.target.classList.toggle('card__like-button_is-active');
+  });
+
+  cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCallback);
+
+  return cardElement;
 }
 
-initialCards.forEach (function (item) {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  
-    cardElement.querySelector('.card__image').src = item.link;
-    cardElement.querySelector('.card__title').textContent = item.name;
-    
-    cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => {
-      evt.target.classList.toggle('card__like-button_is-active');
-    });
+initialCards.forEach ((cardElement) => {
+  cardsContainer.append(createCard(cardElement, deleteCard));
+});
 
-    cardElement.querySelector('.card__delete-button').style.backgroundImage = 'url(../images/delete-icon.svg)';
 
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-
-    cardsContainer.append(cardElement);
-  });
